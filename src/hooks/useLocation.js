94e98817ac346e8cd1f5d44383ebee9c32b6b7f4
Mark Tiddy <marktiddy@gmail.com>
2020-04-25 14:11:07 +0100

    Updated location

--- src/hooks/useLocation.js
+++ src/hooks/useLocation.js
@@ -7,34 +7,44 @@ import {
 
 export default (shouldTrack, callback) => {
   const [err, setErr] = useState(null);
-  const [subscriber, setSubscriber] = useState(null);
 
-  //Helper function for permissions
-  const startWatching = async () => {
-    try {
-      await requestPermissionsAsync();
-      const sub = await watchPositionAsync(
-        {
-          accuracy: Accuracy.BestForNavigation,
-          timeInterval: 1000,
-          distanceInterval: 10,
-        },
-        callback
-      );
-      setSubscriber(sub);
-    } catch (e) {
-      setErr(e);
-    }
-  };
-
+  //We put helper functions that use state objects in use Effect
+  //If we call something from the state we should add it to the dependencies
+  //in the end array of useEffect
   useEffect(() => {
+    let subscriber;
+
+    //Helper function for permissions
+    const startWatching = async () => {
+      try {
+        await requestPermissionsAsync();
+        subscriber = await watchPositionAsync(
+          {
+            accuracy: Accuracy.BestForNavigation,
+            timeInterval: 1000,
+            distanceInterval: 10,
+          },
+          callback
+        );
+      } catch (e) {
+        setErr(e);
+      }
+    };
+
     if (shouldTrack) {
       startWatching();
     } else {
-      subscriber.remove();
-      setSubscriber(null);
+      if (subsriber) {
+        subscriber.remove();
+      }
+      subscriber = null;
     }
-  }, [shouldTrack]);
+    return () => {
+      if (subscriber) {
+        subscriber.remove();
+      }
+    };
+  }, [shouldTrack, callback]);
 
   //convention of hooks is to return an array
   return [err];
